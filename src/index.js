fbb4c7abaabf45e445b404d026f85f5e37c2d178
cvalencia1991 <cesar4a6z@gmail.com>
2022-10-18 23:23:52 -0500

    Create Popup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import './style.css';
 import './imgs/logomenu.jpeg';
+import './imgs/closexbutton.png';
+import './imgs/cali.jpg';
 import getShows from './modules/getShows.js';
 import ShowLibrary from './modules/ShowLibrary.js';
 
 const showContainer = document.getElementById('showList');
+const pruebapop = document.getElementById('prueba');
+const popupinfo = document.getElementById('popupinfo');
 
 getShows().then((result) => {
   const showLib = new ShowLibrary(result);
   showLib.displayShows(6, showContainer);
+});
+
+pruebapop.addEventListener('click', () => {
+  popupinfo.classList.display = 'flex';
+  alert('i press it');
 });
--- src/modules/ShowLibrary.js
+++ src/modules/ShowLibrary.js
@@ -25,13 +25,44 @@ export default class ShowLibrary {
           ${show.genres.join(' ')}
           </div>
               <div>
-                <button>comments</button>
+                <button type="button">comments</button>
               </div>
               <div>
-                <button>Reservations</button>
+                <button type="button">Reservations</button>
               </div>
         </li>`;
       showContainer.insertAdjacentHTML('beforeend', htmlString);
     }
   }
+
+/*  popup(numberOfItems, showContainer) {
+    for (let i = 0; i < numberOfItems; i += 1) {
+      const show = this.ShowArray[i];
+      const htmlString = `
+          <li class="showpopup" id="${show.id}">
+            <img src="${show.image.original}" alt="" class="showImage">
+          <div class="tittleimg">
+            <div><h2 class="showTitle">${show.name}</h2></div>
+            <div class='stylelikes'>
+            <div>
+            <i class="fa-regular fa-heart"></i>
+              <i class="fa-solid fa-heart" style='display:none;'></i>
+            </div>
+             <div>
+             <span class="likeCount">1 like</span>
+             </div>
+            </div>
+          </div>
+            <div class="genres">
+            ${show.genres.join(' ')}
+            </div>
+                <div>
+                  <button id='${show.id}'>comments</button>
+                </div>
+                <div>
+                  <button>Reservations</button>
+                </div>
+          </li>`;
+      showContainer.insertAdjacentHTML('beforeend', htmlString);
+    } */
 }
