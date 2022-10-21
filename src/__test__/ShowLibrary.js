1721bd015be13abb037ee696152aebd51f772a16
cvalencia1991 <cesar4a6z@gmail.com>
2022-10-21 07:39:04 -0500

    Add test Adding items

---
+++ src/__test__/ShowLibrary.js
@@ -0,0 +1,42 @@
+
+export default class ShowLibrary {
+  constructor(Array) {
+    this.ShowArray = Array;
+  }
+
+  displayShows(numberOfItems) {
+    const showContainer = document.getElementById('showList');
+    let count = 0;
+    for (let i = 0; i < numberOfItems; i += 1) {
+      count++;
+      const show = this.ShowArray[i];
+      const htmlString = `
+        <li class="show" id="${show.id}">
+          <img src="${show.image.original}" alt="" class="showImage">
+        <div class="tittleimg">
+          <div><h2 class="${show.id}" showTitle">${show.name}</h2></div>
+          <div class='stylelikes'>
+          <button id='${show.id}' class="likebtn">
+              <span id="${show.id}"><i class="far fa-heart"></i></span>
+              <span id="count-${show.id}"></span> Like
+          </button>
+          </div>
+        </div>
+          <div class="genres">
+          ${show.genres.join(' ')}
+          </div>
+              <div>
+                <button name='${show.id}' type="button">comments</button>
+              </div>
+              <div>
+                <button type="button">Reservations</button>
+              </div>
+              <div>${show.id}</div>
+        </li>`;
+    }
+    const countmovies = document.getElementById('countmovies');
+    countmovies.textContent = count;
+    return count;
+
+  }
+}
--- src/__test__/testitems.test.js
+++ src/__test__/testitems.test.js
@@ -1,42 +1,55 @@
 /** * @jest-environment jsdom */
 
-import postlikes from "../modules/idpost.js";
-import ShowLibrary from "../modules/ShowLibrary.js"
-
+import ShowLibrary from "./ShowLibrary";
 
 describe('testing adding elements',()=>{
    
-    test('add four elements',()=>{
+    it('add four elements',()=>{
+      const showContainer = document.getElementById('showList');
         const show = new ShowLibrary();
         show.ShowArray = [{
             id: '1',
             name: 'Franklin',
-            genres: 'Violence',
+            genres: ['Violence','action'],
             image:{original:'1'}
           },
           {
             id: '2',
             username: 'annonimus',
             comment: 'kalidan the best',
+            genres: ['Violence','action'],
             image:{original:'2'}
           },
           {
             id: '3',
             username: 'abraham',
             comment: 'crazy',
+            genres: ['Violence','action'],
             image:{original:'3'}
           },
           {
             id: '4',
             username: 'claudia',
             comment: 'mom',
+            genres: ['Violence','action'],
             image:{original:'4'}
           }];
-          postlikes(4);
         document.body.innerHTML = '<div>'
             + ' <ul id="showlist"><li></li></ul>'
             + '<div id="countmovies"></div>';
 
-        expect(show.displayShows(3)).tobe(3);
-    })
+        expect(show.displayShows(4)).toBe(4);
+    });
+
+    it('when its empty',()=>{
+        
+      const show = new ShowLibrary();
+      show.ShowArray = [{}];
+      document.body.innerHTML = '<div>'
+          + ' <ul id="showlist"><li></li></ul>'
+          + '<div id="countmovies"></div>';
+
+      expect(show.displayShows(0)).toBe(0);
+  });
+
 });
