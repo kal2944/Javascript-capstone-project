c67ec5b4dad63ff5030a655baccda39a1c7edd46
kalkidan Daniel <99416452+kal2944@users.noreply.github.com>
2022-10-21 19:01:33 -0700

    add comment counter testing

---
+++ src/__test__/commentcounter.test.js
@@ -0,0 +1,26 @@
+import postcomments from './postcomments';
+
+
+describe('Testing comments counter function', () => {
+  test('Count comments', () => {
+    const items = [{
+      item_id: 'item1',
+      username: 'cesar',
+      comment: 'Best one',
+    },
+    {
+      item_id: 'item2', 
+      username: 'kalkidan',
+      comment: 'nice movie',
+    },
+    {
+      item_id: 'item3',
+      username: 'sami',
+      comment: 'i like it',
+    },
+    ];
+    const itesmLength = items.length;
+    const res = postcomments(items);
+    expect(res).toBe(itesmLength);
+  });
+});
